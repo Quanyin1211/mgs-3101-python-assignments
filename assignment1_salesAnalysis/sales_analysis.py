2a8aa6844e05ac463d612792f890e52508f5183b
Quanyin1211 <liqua@kean.edu>
2026-09-23 12:20:31 -0400

    calculate revenue

--- assignment1_salesAnalysis/sales_analysis.py
+++ assignment1_salesAnalysis/sales_analysis.py
@@ -1,26 +1,18 @@
-# Store values into variables
-shop_name = "Vera's Coffee"
-number_of_drinks_sold = 165
-price_per_drink = 4.25
-number_of_pastries_sold = 90
-price_per_pastry = 3.5
+# MGS-3101 Assignment 1 - Sales Analysis
+# Store: Lower Manhattan, Date: 2023-03-27
 
-# Calculate revenue
+
+#store variables
+shop_name = "Lower Manhattan"
+number_of_drinks_sold = 369
+price_per_drink = 2.96
+number_of_pastries_sold = 45
+price_per_pastry = 3.39
+
+#caculate revenue
 drink_revenue = number_of_drinks_sold * price_per_drink
 pastry_revenue = number_of_pastries_sold * price_per_pastry
 total_revenue = drink_revenue + pastry_revenue
+print(drink_revenue)
+print(pastry_revenue)
 print(total_revenue)
-
-# Read and print the txt file
-with open("assignment1_salesAnalysis/report.txt", "r") as file:
-    content = file.read()
-    print(content)
-
-    # Check if total revenue reaches $500 target
-if total_revenue >= 500:
-    print(f"Total revenue is ${total_revenue:.2f}. Great! Meets the $500 target.")
-else:
-    print(f"Total revenue is ${total_revenue:.2f}. Below the $500 target.")
-
-
-

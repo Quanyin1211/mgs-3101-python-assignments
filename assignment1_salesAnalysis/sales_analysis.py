6baca22bd406c94ae2c178aea4c294ac9086d92d
Quanyin1211 <liqua@kean.edu>
2026-09-25 11:50:40 -0400

    revise some problems

--- assignment1_salesAnalysis/sales_analysis.py
+++ assignment1_salesAnalysis/sales_analysis.py
@@ -14,12 +14,9 @@ drink_revenue = number_of_drinks_sold * price_per_drink
 pastry_revenue = number_of_pastries_sold * price_per_pastry
 total_revenue = drink_revenue + pastry_revenue
 
-#write a txt file
-with open("sales_analysis.txt", "w") as f:
-    f.write("Shop: " + shop_name + "\n")
-    f.write("Drink revenue: $" + str(drink_revenue) + "\n")
-    f.write("Pastry revenue: $" + str(pastry_revenue) + "\n")
-    f.write("Total revenue: $" + str(total_revenue) + "\n")
+#print the txt file
+with open("assignment1_salesAnalysis/sales_analysis.txt", "r") as f:
+    print(f.read())
 
 #if/else
 if total_revenue >= 500:
@@ -49,33 +46,6 @@ coffee_beans_count = 531
 loose_tea_count = 381
 branded_count = 349
 packaged_choco_count = 180
-
-with open("sales_analysis.txt", "a") as f:
-    f.write("\n--- Supporting Data ---\n")
-    f.write("bakery attach rate: " + str(round(bakery_attach_rate*100, 1)) + "%\n")
-    f.write("\n")
-    f.write("Monthly sales trend:\n")
-    f.write("  Jan: " + str(jan_sales) + "\n")
-    f.write("  Feb: " + str(feb_sales) + "\n")
-    f.write("  Mar: " + str(mar_sales) + "\n")
-    f.write("  Apr: " + str(apr_sales) + "\n")
-    f.write("  May: " + str(may_sales) + "\n")
-    f.write("  Jun: " + str(jun_sales) + "\n")
-    f.write("Growth (Jan to Jun): " + str(round(growth, 2)) + "times\n")
-    f.write("\n")
-    f.write("Product category comparison:\n")
-    f.write("  Coffee: " + str(coffee_count) + "\n")
-    f.write("  Tea: " + str(tea_count) + "\n")
-    f.write("  Bakery: " + str(bakery_count) + "\n")
-    f.write("  Drinking Chocolate: " + str(drinking_choco_count) + "\n")
-    f.write("  Flavours: " + str(flavours_count) + "\n")
-    f.write("  Coffee beans: " + str(coffee_beans_count) + "\n")
-    f.write("  Loose Tea: " + str(loose_tea_count) + "\n")
-    f.write("  Branded: " + str(branded_count) + "\n")
-    f.write("  Packaged Chocolate: " + str(packaged_choco_count) + "\n")
-
-with open("sales_analysis.txt", "r") as f:
-    print(f.read())
 
 
 
